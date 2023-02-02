e818fb2c60832caa680b92086b5356a58c323200
NoobPro004 <himanshugarg738@gmail.com>
2023-02-03 01:00:49 +0530

    upto 21

---
+++ 21.merge-two-sorted-lists.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode(0);
+         ListNode head=dummy;
+         while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                head.next=list1;
+                list1=list1.next;
+                 head=head.next;
+            }else{
+                head.next=list2;
+                head=head.next;
+                list2=list2.next;
+            }
+         }
+
+         if(list1!=null){
+            head.next=list1;
+         }
+
+         if(list2!=null){
+            head.next=list2;
+         }
+         return dummy.next;
+    }
+}
+// @lc code=end
+
