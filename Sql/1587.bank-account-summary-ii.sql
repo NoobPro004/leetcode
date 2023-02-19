8be8a9d28516b6671a9f12b1c7dec86f206461dc
NoobPro004 <himanshugarg738@gmail.com>
2023-02-19 16:25:39 +0530

    upto 1587

---
+++ Sql/1587.bank-account-summary-ii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select u.name as 'name', sum(t.amount) as 'balance' from Users u inner join Transactions t on u.account=t.account group by u.account having sum(t.amount)>10000;
